1bb0403f35525afb911a2f8f2a0deeca02b82613
Firas <mtibaafiras25@yahoo.com>
2022-03-30 15:32:43 +0200

    Projet fini :D

--- Assets/Script/MenuScripts/ExplodeModelScript.cs
+++ Assets/Script/MenuScripts/ExplodeModelScript.cs
@@ -1,88 +1,158 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Microsoft.MixedReality.Toolkit.UI;
+using Microsoft.MixedReality.Toolkit.Input;
 
 public class ExplodeModelScript : MonoBehaviour
 {
-    private GameObject[] pieces = new GameObject[6];
-    private Vector3[] vecteurs = new Vector3[6];
+    enum State { agglomerated, exploding, exploded, agglomerating };
+
+
+
+    private State etat;
+
+    private GameObject[] pieces = new GameObject[5];
+
+    private Vector3 posInitial = new Vector3();
+
     private Vector3 positionInitiale;
 
+    private Transform camTransform;
+
+    private float angle = 30.0f;
+    private float anglePas = 0.05f;
+    private int index;
 
 
-    private float distance = 1;
-    private int index = 0;
-    private float pas = 0.01f;
 
-    public Transform camTransform;
 
-    bool explosion = false;
 
     // Start is called before the first frame update
     void Start()
     {
+        camTransform = GameObject.FindGameObjectWithTag("MainCamera").transform;
+
         for (int i = 0; i < gameObject.transform.childCount; i++)
         {
             pieces[i] = this.gameObject.transform.GetChild(i).gameObject;
         }
 
-        // Vraiment pas génial
-        vecteurs[0] = new Vector3(1, 0, 0);
-        vecteurs[1] = new Vector3(0.5f, 0, 0.866f);
-        vecteurs[2] = new Vector3(-0.5f, 0, 0.866f);
-        vecteurs[3] = new Vector3(-1, 0, 0);
-        vecteurs[4] = new Vector3(-0.5f, 0, -0.866f);
-        vecteurs[5] = new Vector3(0.5f, 0, -0.866f);
+        EnableScripts(true, true);
+        EnableScripts(false, false);
+
+        index = 0;
+        etat = State.agglomerated;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (explosion)
+        if(etat == State.exploding) Explode();
+        if(etat == State.agglomerating) Agglomerate();
+    }
+
+    public void ModelDisplacement()
+    {
+        Debug.Log("sa rentre dedans");
+        if (etat == State.agglomerated)
         {
-            ExplodeModel();
+            etat = State.exploding;
+            posInitial = camTransform.position;
+
+            EnableScripts(true, false);
+
+        }
+        else if (etat == State.exploded)
+        {
+            etat = State.agglomerating;
+            EnableScripts(false, false);
         }
     }
 
-    public void ExplodeModel()
+    public void Explode()
     {
-        if (index == 0)
+        Debug.Log(index * anglePas);
+        if (index * anglePas <= angle)
         {
-            explosion = true;
-            positionInitiale = gameObject.transform.position;
-            gameObject.transform.position = camTransform.position - new Vector3(0, 0.2f, 0);
-            Debug.Log(pieces.Length);
-            for (int i = 0; i < pieces.Length; i++)
-            {
-                pieces[i].transform.localPosition = new Vector3(0,0,0);
-            }
-        }
-        else
-        {
-            for (int i = 0; i < pieces.Length; i++)
-            {
-                pieces[i].transform.localPosition += vecteurs[i] * distance * pas;
-            }
-        }
+            pieces[1].transform.RotateAround(posInitial, Vector3.up, anglePas);
+            pieces[2].transform.RotateAround(posInitial, Vector3.up, -anglePas);
+            pieces[3].transform.RotateAround(posInitial, Vector3.up, anglePas * 2);
+            pieces[4].transform.RotateAround(posInitial, Vector3.up, -anglePas * 2);
 
-        if((float)((index-1)*pas) == distance)
-        {
-            Debug.Log("if");
-            explosion = false;
-            index = 0;
-        }
-        else
-        {
-            Debug.Log("else");
             index++;
         }
-    }
+       
 
+        else
+        {
+            etat = State.exploded;
+            index = 0;
+            EnableScripts(false, true);
+        }
+
+        Debug.Log("sorti");
+    }
     public void Agglomerate()
     {
-        
+        if (index == 0) 
+        {
+            for (int i = 0; i < pieces.Length - 1; i++)
+            {
+                pieces[i].transform.GetChild(0).transform.localEulerAngles = new Vector3(0, 0, 0);
+            }
+        }
+        if (index * anglePas <= angle)
+        {
+            pieces[1].transform.RotateAround(posInitial, Vector3.up, -anglePas);
+            pieces[2].transform.RotateAround(posInitial, Vector3.up, anglePas);
+            pieces[3].transform.RotateAround(posInitial, Vector3.up, -anglePas * 2);
+            pieces[4].transform.RotateAround(posInitial, Vector3.up, anglePas * 2);
 
+            for (int i = 0; i < pieces.Length - 1; i++)
+            {
+                pieces[i].transform.GetChild(0).transform.localPosition/= 1.01f;
+                pieces[i].transform.GetChild(0).transform.localScale += (new Vector3(1, 1, 1) - pieces[i].transform.GetChild(0).transform.localScale)/100.0f;
+            }
+
+            index++;
+        }
+        else
+        {
+            etat = State.agglomerated;
+            index = 0;
+
+            EnableScripts(true, true);
+
+
+            for (int i = 0; i < pieces.Length - 1; i++)
+            {
+                pieces[i].transform.GetChild(0).transform.localPosition = new Vector3(0, 0, 0);
+                pieces[i].transform.GetChild(0).transform.localScale = new Vector3(1, 1, 1);
+            }
+        }
+    }
+    
+    // Désactive ou active les script permettant l'interaction avec les pieces ou la pièce assemblée parente
+    public void EnableScripts(bool parent, bool enable)
+    {
+        if(parent)
+        {
+            transform.GetChild(0).GetComponent<ModelBoundingBox>().enabled = false;
+            transform.GetChild(0).GetComponent<ObjectManipulator>().enabled = false;
+            transform.GetChild(0).GetComponent<NearInteractionGrabbable>().enabled = false;
+        }
+        else
+        {
+            for (int i = 0; i < pieces.Length - 1; i++)
+            {
+                pieces[i].transform.GetChild(0).GetComponent<ModelBoundingBox>().enabled = enable;
+                pieces[i].transform.GetChild(0).GetComponent<ObjectManipulator>().enabled = enable;
+                pieces[i].transform.GetChild(0).GetComponent<NearInteractionGrabbable>().enabled = enable;
+            }
+        }
         
     }
-
 }
+  
+
